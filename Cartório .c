16fc6c3d733c3dfa51a97842bda65cff9a414505
rockrocha <kauanerocha@yahoo.com.br>
2024-09-19 19:34:05 -0300

    Opção de Saida
    Inclusao de sair do usuario

--- Cartório .c
+++ Cartório .c
@@ -125,6 +125,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n"); 
+		printf("\t4 - Sair do Sistema\n\n");
 		printf("Opcao: "); //Fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -144,6 +145,12 @@ int main()
 			case 3:
 			deletar();
 			break;
+			
+			case 4:
+			printf("Obrigado por ultilizar o sistema\n");
+			return 0;
+			break;
+			
 				
 			default:
 			printf("Essa opção não está disponivel!\n");
